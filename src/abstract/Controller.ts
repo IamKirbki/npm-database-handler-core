b66116ec51e18ee822c0a7b02c0c8b93b43c2a0f
Daimy Wisselink <134633469+IamKirbki@users.noreply.github.com>
2026-01-18 14:04:01 +0100

    Update Controller.ts

--- src/abstract/Controller.ts
+++ src/abstract/Controller.ts
@@ -1,8 +1,15 @@
 export default abstract class Controller<Type extends columnType> {
     abstract async index(): Promise<Model<Type>[]>;
-    abstract async show(value: string | number): Promise<Model<Type>>;
-    abstract async edit(value: string | number): Promise<Model<Type>>;
+    abstract async show(value: string | number | UUIDType): Promise<Model<Type>>;
+    abstract async edit(value: string | number | UUIDType): Promise<Model<Type>>;
     abstract async update(id: string | number | UUIDType, newValues: Type): Promise<Model<Type>>;
     abstract async create(data: Type): Promise<Model<Type>>;
     abstract astnc delete(id: string | number | UUIDType): Promise<boolean>;
+
+    public static async index<StaticType extends columnType>(
+        this: new () => Controller<StaticType>
+    ): Model<StaticType> {
+        const instance = new this();
+        return await instance.index();
+    }
 }
